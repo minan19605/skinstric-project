2af00d5f6b12615c61b2b31cd841f6fec3b50392
minan19605 <minan.19605@gmail.com>
2025-12-07 11:26:32 -0600

    Submit successful process

---
+++ components/WaveDot.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './WaveDot.module.css'
+
+export default function WaveDot() {
+  return (
+    <div className={styles.dot_wave}>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+  )
+}
