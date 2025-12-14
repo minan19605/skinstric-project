40cc69cfb07dbbbae52b856aee4648ac2087f7ca
minan19605 <minan.19605@gmail.com>
2025-12-13 23:21:06 -0600

    Add dynamic create page for Summary

--- app/select/page.tsx
+++ app/select/page.tsx
@@ -18,12 +18,14 @@ export default function Page() {
 
     <div className={styles.container}>
         <div className={styles["content__wrapper"]}>
+            <Link href="/summary">
             <div className={`${styles.content} cursor-pointer! bg-gray-200!`}>
                 <p className={styles["description"]}>EMOGRAPHICS</p>
                 <div className={`${styles.square__wrapper} ${styles.square_1}`}>
                     <div className={`${styles.square}`}></div>
                 </div>
             </div>
+            </Link>
             <div className={styles.content}>
                 <p className={styles["description"]}>SKIN TYPE DETAILS</p>
                 <div className={styles["square__wrapper"]}>
@@ -46,7 +48,7 @@ export default function Page() {
     </div>
 
     <div className={styles.row_wrapper}>
-        <Link href="/testing">
+        <Link href="/result">
             <div className={styles["left-btn__wrapper"]}>
                 <div className={styles["btn-frame"]}>
                     <FaPlay size={12} className={styles["left-play"]}/>
@@ -55,12 +57,12 @@ export default function Page() {
                 <div className={styles["btn-content"]}>BACK</div>
             </div>
         </Link>
-        <Link href="/">
+        <Link href="/summary">
             <div className={styles["right-btn__wrapper"]} >
-                <div className={styles["btn-content"]}>HOME</div>
+                <div className={styles["btn-content"]}>GET SUMMARY</div>
                 <div className={styles["btn-frame"]}>
                     <FaPlay size={12} className={styles["right-play"]}/>
-                    <div className={styles["inner-content"]}>HOME</div>
+                    <div className={styles["inner-content"]}>SUM</div>
                 </div>
             </div>
         </Link>
